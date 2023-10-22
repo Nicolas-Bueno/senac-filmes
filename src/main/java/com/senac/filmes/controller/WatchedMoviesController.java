bc1d02472d2182259e99bc2766c8441fc2859322
Nicolas Bueno <nicolasbrian202@gmail.com>
2023-10-22 19:41:10 -0300

    create html de cadastro

--- src/main/java/com/senac/filmes/controller/WatchedMoviesController.java
+++ src/main/java/com/senac/filmes/controller/WatchedMoviesController.java
@@ -4,6 +4,8 @@ import com.senac.filmes.model.Filme;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  *
@@ -16,5 +18,11 @@ public class WatchedMoviesController {
     public String cadastrarFilme(Model model){
         model.addAttribute("filme", new Filme());
         return "cadastro";
+    }
+    
+    @PostMapping("/cadastro")
+    public String receberCadastro(Model model, @ModelAttribute Filme filme){
+        model.addAttribute("filme", filme);
+        return "filme";
     }
 }
